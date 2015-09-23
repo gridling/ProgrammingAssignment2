8bb7fd8f506a7855c9abad213980d2cefe3ff8c3
gridling <gridling@gmail.com>
2015-09-24 04:55:58 +0800

    Assignment 2 of Coursera R programming
    create a cache for a matrix and its inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall description: 
+ # Create a square matrix with n dimensions, 
+ #   each of the elements is randomly arranged from a sequence 1 to n^2.
+ # The square matrix and its inverse matrix are stored within the object 
+ #   created by the function "makeCacheMatrix()"
 
-## Write a short comment describing this function
+## makeCacheMatrix()
+ # Create a n-by-n square matrix object
+ #   with each of the elements randomly arranged from 1 to n^2
+ # Cache its inverse matrix in a variable named "inv"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(n) {
+    
+    inv <- NULL
+    x = matrix(sample(n^2), n, n)
+ 
+    getmatrix <- function() x 
+    setinv <- function(inverted) inv <<- inverted
+    getinv <- function() inv
+    
+    list(getmatrix = getmatrix, 
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve()
+ # Computes the inverse matrix of the square matrix 
+ #   which had been created by and stored in the function makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message("getting the cashed inverse matrix")
+        return(inv)
+    }
+    matrix <- x$getmatrix()
+    inv <- solve(matrix)
+    x$setinv(inv)
+    inv
 }
+
